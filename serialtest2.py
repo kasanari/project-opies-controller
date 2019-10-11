d432848c83be6a423f44759e73c9ecbfed26af86
Torun <torun.nicander@gmail.com>
2019-10-11 15:28:42 +0200

    can get POS values from shell

--- serialtest2.py
+++ serialtest2.py
@@ -11,11 +11,20 @@ ser.write(b'\r\r')  # Enter key twice to enter shell mode
 res = ser.read(10)  # Read 10 bytes
 time.sleep(0.5)
 ser.write(b'si\r')  # System information pls
+time.sleep(0.5)
+# ser.write(b'acts 0 1 0 1 0 1 1 2 0\r')
+
+f = open("location_data.txt", "w+")
 
 print(res)
-
-while True:
-    ser.write(b'help\r')
+ser.write(b'lep\r')
+for i in range(100):
+    print('hh')
     res = ser.read(100)
     if len(res) > 0:
-        print(res.decode("utf-8"))
+        res_decoded = res.decode("utf-8")
+        print(res_decoded)
+        # Regular expression. only write those that match POS
+        f.write(res_decoded)
+
+f.close()
