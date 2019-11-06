1a3b02b4e79cb29e42f684c0d6f8e9600658b32c
Jakob Nyberg <jakobnnyberg@gmail.com>
2019-11-06 11:23:12 +0100

    add connect/disconnect function

--- static/socket_handler.js
+++ static/socket_handler.js
@@ -1,21 +1,46 @@
-var ws = new WebSocket("ws://" + location.hostname + ":5678/");
+var connected = false;
+var ws;
 
-ws.onmessage = function (event) {
+function addMessage(message_content) {
     var message_holder = document.getElementById("messages"),
-        message = document.createElement('p'),
-        content = document.createTextNode(event.data);
+    message = document.createElement('p'),
+    content = document.createTextNode(message_content);
     message.id = "message";
     message.appendChild(content);
     message_holder.appendChild(message);
-};
+    message_holder.scrollTop = message_holder.scrollHeight
+}
+
+function eventHandler(event) {
+    addMessage(event.data)
+}
 
 function sendOrder() {
-    var formData = new FormData(document.querySelector('form'));
-    var destination = {};
-    destination.x = formData.get("x_destination");
-    destination.y = formData.get("y_destination");
-    destination.speed = formData.get("drive_speed");
-    destination.angle = formData.get("wheel_angle");
-    let json = JSON.stringify(destination);
-    ws.send(json)
+    if (connected) {
+        var formData = new FormData(document.querySelector('form'));
+        var destination = {};
+        destination.x = formData.get("x_destination");
+        destination.y = formData.get("y_destination");
+        destination.speed = formData.get("drive_speed");
+        destination.angle = formData.get("wheel_angle");
+        let json = JSON.stringify(destination);
+        ws.send(json)
+    } else {
+        console.log("WebSocket not connected!")
+    }
+}
+
+function connect() {
+    ws = new WebSocket("ws://" + location.hostname + ":5678/");
+
+    connected = true;
+
+    ws.onmessage = eventHandler;
+    addMessage("Connected to host.")
+}
+
+function disconnect() {
+    ws.close();
+    connected = false;
+    addMessage("Disconnected from host.")
 }
