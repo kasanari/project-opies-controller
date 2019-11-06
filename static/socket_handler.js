0d308535fe9a5e9a721be192ed2e23e277e9820a
Jakob Nyberg <jakobnnyberg@gmail.com>
2019-11-06 08:43:48 +0100

    make client prettier

--- static/socket_handler.js
+++ static/socket_handler.js
@@ -1,8 +1,12 @@
 var ws = new WebSocket("ws://" + location.hostname + ":5678/");
 
 ws.onmessage = function (event) {
-    var message = document.getElementById("text");
-    message.textContent = event.data
+    var message_holder = document.getElementById("messages"),
+        message = document.createElement('p'),
+        content = document.createTextNode(event.data);
+    message.id = "message";
+    message.appendChild(content);
+    message_holder.appendChild(message);
 };
 
 function sendOrder() {
@@ -10,6 +14,8 @@ function sendOrder() {
     var destination = {};
     destination.x = formData.get("x_destination");
     destination.y = formData.get("y_destination");
+    destination.speed = formData.get("drive_speed");
+    destination.angle = formData.get("wheel_angle");
     let json = JSON.stringify(destination);
     ws.send(json)
 }
