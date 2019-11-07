000749702ebfd2156964d64835b61b4b690f591c
Jakob <jakobnnyberg@gmail.com>
2019-11-07 15:59:27 +0100

    make motor work

--- motor_control.py
+++ motor_control.py
@@ -7,6 +7,7 @@ class Servo:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(pin, GPIO.OUT)
         self.pwm = GPIO.PWM(pin, 50)
+        self.neutral = 7.5
 
     def stop(self):
         self.pwm.stop()
@@ -18,7 +19,6 @@ class Steering(Servo):
         super().__init__(pin)
         self.max_angle = max_angle
         self.min_angle = min_angle
-        self.neutral = (max_angle+min_angle)/2
         self.pwm.start(self.neutral)
 
 
@@ -29,13 +29,27 @@ class Steering(Servo):
         elif angle < self.min_angle:
             warnings.warn(f"Angle of {angle} is under min_angle of {self.min_angle}!")
             angle = self.min_angle
-        self.pwm.ChangeDutyCycle(angle)  # TODO make this more sophisticated
+        print(f"Wheel angle changed to {angle}.")
+        self.pwm.ChangeDutyCycle(angle)
 
 
 class Motor(Servo):
 
+    def __init__(self, pin, max_back_speed=7, max_forward_speed=8):
+        super().__init__(pin)
+        self.pwm.start(self.neutral)
+        self.max_fspeed = max_forward_speed
+        self.max_bspeed = max_back_speed
+
     def neutral(self):
-        self.pwm.ChangeDutyCycle(90)  # TODO make this more sophisticated
+        self.pwm.ChangeDutyCycle(7.5)
 
     def set_speed(self, speed):
+        if speed > self.max_fspeed:
+            speed = self.max_fspeed
+            warnings.warn(f"{speed} exceeds max speed of {self.max_fspeed}!")
+        elif speed < self.max_bspeed:
+            speed = self.max_bspeed
+            warnings.warn(f"{speed} is under min_angle of {self.max_bspeed}!")
+        print(f"Speed changed to {speed}.")
         self.pwm.ChangeDutyCycle(speed)
