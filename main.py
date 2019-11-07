0b55f92c89889e3750ac94ee901fdef2674eac6c
Jakob <jakobnnyberg@gmail.com>
2019-11-07 13:48:47 +0100

    move async stuff to main

--- main.py
+++ main.py
@@ -2,8 +2,46 @@ import location_server
 from motor_control import Steering, Motor
 import json
 import asyncio
+import random
+import datetime
+from websockets import WebSocketServerProtocol
 
 #steering = Steering(17, 4, 13)
+
+async def time():  # TODO change this to send location info as well as time
+    await asyncio.sleep(1)
+    data_to_send = {
+        'x': random.randint(0, 10),
+        'y': random.randint(0, 10),
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+
+    return json.dumps(data_to_send)
+
+
+async def time_handler(websocket, path):
+    print("Client Connected!")
+    while True:
+        message = await time()
+        await websocket.send(message)
+
+
+async def order_handler(websocket, path, queue):
+    async for message in websocket:
+        await queue.put(message)
+
+
+async def handler(websocket: WebSocketServerProtocol, path: str):
+    queue = asyncio.Queue()
+    consumer_task = asyncio.create_task(order_handler(websocket, path, queue))
+    producer_task = asyncio.create_task(time_handler(websocket, path))
+    motor_task = asyncio.create_task(motor_control_task(queue))
+
+    done, pending = await asyncio.wait([consumer_task, producer_task, motor_task], return_when=asyncio.FIRST_COMPLETED)
+    print("Client Disconnected!")
+    for task in pending:
+     task.cancel()
+
 
 async def motor_control_task(queue):
     steering = Steering(17, 4, 13)
@@ -16,6 +54,7 @@ async def motor_control_task(queue):
             print(message)
             angle = float(message["angle"])
             steering.set_angle(angle)
+
 
     except asyncio.CancelledError:
         print("Motor task cancelled")
@@ -35,7 +74,7 @@ if __name__== "__main__":
 
     try:
         location_server.start_web_client()
-        start_server = location_server.start_websocket_server()
+        start_server = location_server.start_websocket_server(handler)
         asyncio.get_event_loop().run_until_complete(start_server)
         asyncio.get_event_loop().run_forever()
 
