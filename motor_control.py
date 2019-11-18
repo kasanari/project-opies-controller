2cd09fa51e693d14a2e2e38f141e01d37e8baccf
Jakob <jakobnnyberg@gmail.com>
2019-11-18 13:11:16 +0100

    interfacing fixes

--- motor_control.py
+++ motor_control.py
@@ -72,12 +72,16 @@ async def motor_control_task(web_queue, from_serial_queue: asyncio.Queue):
             message_type = message["type"]
 
             if message_type == "car_control":
+                if auto_steer is not None:
+                    auto_steer.cancel()
                control_car_from_message(rc_car, message)
 
             elif message_type == "destination":
                 x_destination = message["x"]
                 y_destination = message["y"]
-                destination = {'x': int(x_destination), 'y':int(y_destination)}
+                destination = {'x': float(x_destination), 'y': float(y_destination)}
+                if auto_steer is not None:
+                    auto_steer.cancel()
                 auto_steer = asyncio.create_task(auto_steer_task(rc_car, destination, from_serial_queue))
 
             elif message_type == 'stop':
