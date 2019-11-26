39f16267f9afd87cc133c9be4a6c5138aac0239f
Jakob <jakobnnyberg@gmail.com>
2019-11-26 13:18:31 +0100

    use steeringcontroller

--- auto_steering.py
+++ auto_steering.py
@@ -1,6 +1,7 @@
 from location_data_handler import LocationData, Transform
 from pidcontroller import PIDController
 from arduino_interface import arduino_serial
+from steering_controller import SteeringController
 import numpy as np
 import pandas as pd
 import asyncio
@@ -10,16 +11,24 @@ def approximate_angle(length, speed, wheel_angle):
     return (speed * np.tan(wheel_angle)) / length
 
 
+async def check_for_collision(limit):
+    distance = await arduino_serial.distance_measure()
+    print(f"Distance in CM: {distance}")
+
+    if distance < limit:
+        return True
+
+
 class AutoPilot:
 
-    def __init__(self, target: Transform):
+    def __init__(self, target: LocationData, angle):
         self.target = target
         self.data_log = pd.DataFrame()
         self.prev_location = Transform(0, 0, 0)
-        self.target_angle = 45
+        self.target_angle = angle
         self.time = 0
 
-    def calculate_errors(self, position: Transform, angle):
+    def calculate_errors(self, position: LocationData, angle):
         x_diff = self.target.x - position.x
         y_diff = self.target.y - position.y
         x = np.cos(angle) * x_diff + np.sin(angle) * y_diff
@@ -39,27 +48,21 @@ class AutoPilot:
         angle = (np.arctan2(y_diff, x_diff) - np.pi / 2) * -1
         return np.rad2deg(angle)
 
-    def check_for_collision(self, limit):
-        distance = await arduino_serial.distance_measure()
-        print(f"Distance in CM: {distance}")
-
-        if distance < limit:
-            return True
-
-    async def auto_steer_task(self, rc_car, from_serial_queue, distance_control=True):
+    async def auto_steer_task(self, rc_car, from_serial_queue, distance_control: bool =False):
 
         print(f"Going to ({self.target.x}, {self.target.y})")
         loop = asyncio.get_running_loop()
 
         speed_controller = PIDController(K_p=0.2, K_d=0.02, K_i=0.00005)
-        steering_controller = PIDController(K_p=0.2, K_d=0.02, K_i=0.00005)
+        steering_controller = SteeringController()
+        wheel_angle = 0
 
         try:
             while True:
 
                 if distance_control:
-                    collision_imminent = self.check_for_collision(limit=40)
-                    if (collision_imminent):
+                    collision_imminent = await check_for_collision(limit=40)
+                    if collision_imminent:
                         print("Stopping due to wall.")
                         rc_car.brake()
                         rc_car.stop()
@@ -67,19 +70,28 @@ class AutoPilot:
 
                 location: LocationData = await from_serial_queue.get()
 
-                e_angle = self.angle_error(location.x, location.y)
-                e_pos = self.position_error(location.x, location.y)
+                speed = self.approximate_speed(location, loop.time())
+                heading_angle = approximate_angle(2.65, speed, wheel_angle)
 
-                acceleration = speed_controller.get_control_signal(e_pos, loop.time(), P=True, D=True, I=False)
-                angle = steering_controller.get_control_signal(e_angle, loop.time())
+                print(f"speed: {speed}")
+                print(f"heading_angle: {heading_angle}")
+
+                x_e, y_e, angle_e = self.calculate_errors(location, heading_angle)
+
+                print(f"x_e: {x_e}")
+                print(f"y_e: {y_e}")
+                print(f"angle_e: {angle_e}")
+
+                acceleration = speed_controller.get_control_signal(y_e, loop.time(), P=True, D=True, I=False)
+                wheel_angle = steering_controller.get_control_signal(speed, angle_e, x_e, loop.time())
 
                 print(f"acceleration: {acceleration}")
-                print(f"angle: {angle}")
+                print(f"wheel_angle: {wheel_angle}")
 
-                rc_car.set_wheel_angle(angle)
+                rc_car.set_wheel_angle(wheel_angle)
                 rc_car.set_acceleration(acceleration)
 
-                self.logger(y_loc=location.y, error=e_pos)
+                #self.logger(y_loc=location.y, error=e_pos)
                 self.prev_location = Transform(location.x, location.y, 0)
 
                 print(f"---- {loop.time()} ----")
@@ -91,11 +103,13 @@ class AutoPilot:
 
         except Exception as e:
             print(e)
+            rc_car.stop()
+            return
 
     def logger(self, **kwargs):
         # location = location.get_as_dict()
         time_stamp = pd.Timestamp.utcnow()
         self.data_log = self.data_log.append(pd.DataFrame(kwargs, index=[time_stamp]))
 
-    def approximate_speed(self, current_location: Transform, current_time):
+    def approximate_speed(self, current_location: LocationData, current_time):
         return (self.prev_location.x - current_location.x) / (self.time - current_time)
