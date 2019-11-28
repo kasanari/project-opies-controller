cbc95ba4b9f114b17d136696c95e80898c66237a
Torun <torun.nicander@gmail.com>
2019-11-28 13:49:37 +0100

    kalman Q and R better. still some lag

--- kalman_filtering.py
+++ kalman_filtering.py
@@ -4,65 +4,50 @@ import numpy as np
 from scipy.linalg import block_diag
 from location_data_handler import LocationData
 
-# class KalmanHelper:
-#
-#     def __init__(self, loc_data):
-#         self.x_vector = np.array(loc_data.x)
-#         self.x_dot = np.array([0])
-#         self.y_vector = np.array(loc_data.y)
-#         self.y_dot = np.array([0])
-#         self.state_vector = np.array(self.x_vector, self.y_vector, self.x_dot, self.y_dot)  # state vector. in KF
-#         # theory, called x
-#
-#     def update_loc_vectors(self, loc_data):
-#         self.x_vector = np.append(self.x_vector, loc_data.x)
-#         self.y_vector = np.append(self.y_vector, loc_data.y)
-#
-#     def update_velocities(self, past_loc, current_loc, delta):
-#         new_x_dot = (current_loc.x-past_loc.x)/delta
-#         new_y_dot = (current_loc.y-past_loc.y)/delta
-#         self.x_dot = np.append(self.x_dot, new_x_dot)
-#         self.y_dot = np.append(self.y_dot, new_y_dot)
-#
-#     def update_state_vector(self):
-#         self.state_vector = np.array(self.x_vector, self.y_vector, self.x_dot, self.y_dot)
-
 
 # var_x and var_y in meters
 def init_kalman_filter(loc_data, dt, covar_x_y=0.2):
     kf = KalmanFilter(dim_x=4, dim_z=2)  
     # init state vector x: [x, y, x_dot, y_dot]
     kf.x = np.array([loc_data.x, loc_data.y, 0.0, 0.0])  # initialize with first loc_data x and y, 0 in velocity
-    kf.F = np.array([[1., 0., dt, 0],
-                    [0., 1., 0, dt],
-                    [0., 0., 1., 0.],
-                    [0., 0., 0., 1.]])
+    kf.F = set_F(dt)
     kf.H = np.array([[1., 0., 0., 0.],
                     [0., 1., 0., 0.]])
     distrust_in_value = calculate_distrust(loc_data.quality)
     kf.P *= distrust_in_value
 
-    # P is a unit matrix. Multiply it with quality factor of first measurement. Maybe higher value?
-      # measurement noise. not sure what values to put here.
-    kf.R = measurement_noise_update(distrust_value=distrust_in_value, covar_x_y=covar_x_y)
-    #kf.R = measurement_noise_update(distrust_value=5, covar_x_y=covar_x_y)
-    q = Q_discrete_white_noise(dim=2, dt=dt, var=2)
-    # TODO "For simplicity I will assume the noise is a discrete time Wiener process - that it is constant for each time
-    # period. This assumption allows me to use a variance to specify how much I think the model changes between steps"
-    # soooo change var?? idk. maybe should reflect the update_rate. Probably doesn't change much each 10Hz?
-    kf.Q = block_diag(q, q)  # process noise x and y. TODO should probably change this to reflect the covariance i assume elsewhere
-    #Q =[[0., var, 0., 0.]
-    #   [var, var, 0., 0.]
-    #   [0., 0., 0., var]
-    #   [0., 0., var, var]]
+    kf.R = measurement_noise_update(distrust_value=distrust_in_value, covar_x_y=covar_x_y)  # measurement noise
+    kf.Q = set_Q(dt)  # process noise TODO should probably change this to reflect the covariance i assume elsewhere
+
     return kf
+
+
+def set_F(dt):
+    f = np.array([[1., 0., dt, 0],
+                  [0., 1., 0, dt],
+                  [0., 0., 1., 0.],
+                  [0., 0., 0., 1.]])
+    return f
+
+
+def set_Q(dt, acceleleration=True, var_x=0.0, var_y=0.0, var_x_dot=0.0, var_y_dot=0.0,
+          covar_xy=0.0, covar_xxdot=0.0, covar_xydot=0.0, covar_yxdot=0.0,
+          covar_yydot=0.0, covar_xdotydot=0.0):
+    if acceleleration:
+        var_x_dot = np.square(dt)
+        var_y_dot = np.square(dt)
+    q = np.array([[var_x, covar_xy, covar_xxdot, covar_xydot],
+                  [covar_xy, var_y, covar_yxdot, covar_yydot],
+                  [covar_xxdot, covar_yxdot, var_x_dot, covar_xdotydot],
+                  [covar_xydot, covar_yydot, covar_xdotydot, var_y_dot]])
+    return q
 
 
 def calculate_distrust(quality):
     if quality == 0:
         quality = 0.01
     accuracy_UWB_localisation = 0.1  # m
-    distrust = accuracy_UWB_localisation + 2 * (1000 / quality)
+    distrust = accuracy_UWB_localisation + quality/10
     # ex measurement: x = 5±0.1 if quality is 100, we want trust_in_measurement to be 1.1
     return distrust
 
@@ -106,7 +91,9 @@ def measurement_noise_update(distrust_value, covar_x_y=0.2):
 # If the loc_data is None we keep the last z, but we make the covariance matrix for the measurements
 # have ~infinity numbers, so the prediction is vastly favored over the measurement.
 # Returns: a location estimate as a LocationData
-def kalman_updates(kf, loc_data):
+def kalman_updates(kf, loc_data, timestep):
+    kf.F = set_F(timestep)
+    kf.Q = set_Q(timestep)
     if loc_data is not None:
         z = measurement_update(loc_data)
         distrust_in_measurement = calculate_distrust(loc_data.quality)
@@ -119,23 +106,7 @@ def kalman_updates(kf, loc_data):
         loc_quality = -99
     kf.predict()
     kf.update(z)
+
     filtered_loc = LocationData(x=kf.x[0], y=kf.x[1], z=0, quality=loc_quality)
 
     return filtered_loc  
-
-
-def do_extended_kalman_filter(kalman_helper, x_state_vector):
-    dim_x = 4  # state variables. tracking x,y,x_dot,y_dot = 4 state variables
-    dim_z = 2  # measurement inputs. (x,y) = 2
-    kf = ExtendedKalmanFilter(dim_x, dim_z)
-    kf.x_state_vector == kalman_helper.state_vector
-
-
-
-
-
-def get_state_vector(loc_as_dataframe):
-    # want to return x_kf = [x y x_dot y_dot]
-
-    np.array()
-    return None
