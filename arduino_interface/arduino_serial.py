3558fbefeb4cf96534a3b50e8d1edf39c404ab15
Jakob <jakobnnyberg@gmail.com>
2019-12-03 08:28:48 +0100

    prints comma seperated values

--- arduino_interface/arduino_serial.py
+++ arduino_interface/arduino_serial.py
@@ -1,10 +1,19 @@
 import serial
+import time
+import json
+
+import csv
+import io
+
+def read_IMU(connection):
+    pass
+
 
 def connect_to_arduino():
     try:
-        ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=2)
+        ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=2)
     except serial.SerialException:
-        ser = serial.Serial('/dev/ttyUSB1', 9600, timeout=2)
+        ser = serial.Serial('/dev/ttyUSB1', 115200, timeout=2)
     return ser
 
 def measure_distance(connection):
@@ -22,9 +31,33 @@ if __name__ == "__main__":
     try:
         a_ser = connect_to_arduino()
 
+        for _ in range(1, 6):
+            line = a_ser.readline()
+            print(line)
+
+        a_ser.write(b'a\n')
+
+        time.sleep(5)
+
         while 1:
-            distance = measure_distance(a_ser)
-            print(distance)
+            msg = "a\n".encode()
+            a_ser.write(msg)
+
+            for i in range(0, 2):
+                line = a_ser.readline().strip()
+                line = line.replace(b'\x00', b'')
+                line_str = line.decode()
+
+                reader = csv.reader(
+                    io.StringIO(line_str),
+                    delimiter=',',
+                    quotechar='"',
+                    skipinitialspace=True,
+                )
+
+                for row in reader:
+                    print(row)
+            print("--------")
 
     except KeyboardInterrupt:
         print("Stopping")
