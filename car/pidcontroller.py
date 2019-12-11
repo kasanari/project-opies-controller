0e9eea8abf10545d200b4b6c7c770404850f62bd
Jakob <jakobnnyberg@gmail.com>
2019-12-11 14:42:08 +0100

    comment out some prints

--- car/pidcontroller.py
+++ car/pidcontroller.py
@@ -26,25 +26,25 @@ class PIDController:
         e = error
         control_signal = 0
 
-        print(f"error: {e}")
+        #print(f"error: {e}")
 
         delta_t = abs(current_time - self.time)
         self.time = current_time
 
-        print(f"delta_t = {delta_t}")
+        #print(f"delta_t = {delta_t}")
 
         if P:
             p = self.K_p * e
-            print(f"p: {p}")
+            #print(f"p: {p}")
             control_signal += p
         if D:
             d = self.K_d * (e - self.prev_e)/delta_t
             control_signal += d
-            print(f"d: {d}")
+            #print(f"d: {d}")
         if I:
             i = self.K_i * self.sum_e
             control_signal += i
-            print(f"i: {i}")
+            #print(f"i: {i}")
 
         self.sum_e += e*delta_t
         self.prev_e = e
