efb82c5218f3154791d9d02624082b337b2a9fb9
Jakob Nyberg <jakobnnyberg@gmail.com>
2019-12-18 11:03:49 +0100

    simple pure pursuit

--- pathfinding/pathing.py
+++ pathfinding/pathing.py
@@ -1,13 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+from dataclasses import dataclass
+import pathfinding.pure_pursuit as pp
+import math
+
+@dataclass
+class Path:
+    x: np.ndarray
+    y: np.ndarray
+
+def bresenham(x0, y0, x1, y1):
+    """Yield integer coordinates on the line from (x0, y0) to (x1, y1).
+    Input coordinates should be integers.
+    The result will contain both the start and the end point.
+    """
+    dx = x1 - x0
+    dy = y1 - y0
+
+    xsign = 1 if dx > 0 else -1
+    ysign = 1 if dy > 0 else -1
+
+    dx = abs(dx)
+    dy = abs(dy)
+
+    if dx > dy:
+        xx, xy, yx, yy = xsign, 0, 0, ysign
+    else:
+        dx, dy = dy, dx
+        xx, xy, yx, yy = 0, ysign, xsign, 0
+
+    D = 2*dy - dx
+    y = 0
+
+    for x in range(dx + 1):
+        yield x0 + x*xx + y*yx, y0 + x*xy + y*yy
+        if D >= 0:
+            y += 1
+            D -= 2*dx
+        D += 2*dy
+
 
 def line(x0, y0, x1, y1):
 
     vals = np.linspace(x0, x1, 10)
     path = np.interp(vals, [x0, x1], [y0, y1])
     return [vals, path]
-
 
 
 def plot_line(x0, y0, x1, y1):
@@ -47,18 +85,43 @@ def create_path_from_points(x_points, y_points):
 
         path_x, path_y = plot_line(x_points[n], y_points[n], x_points[n+1], y_points[n+1])
         plt.plot(path_x, path_y, 'x')
-        total_path_x = np.concatenate(total_path_x, path_x, axis=1)
-        total_path_y = np.concatenate(total_path_y, path_y, axis=1)
+
+        total_path_x = np.concatenate((total_path_x, path_x), axis=None)
+        total_path_y = np.concatenate((total_path_y, path_y), axis=None)
 
     plt.plot(x_points, y_points, 'o')
-    plt.plot(total_path_x, total_path_y)
+
+    path = Path(total_path_x, total_path_y)
+
+    x, y = 0.5, 1.25
+    yaw = np.deg2rad(0)
+    l = 0.5
+
+    tx, ty = pp.find_nearest_point(x, y, l, path)
+    alpha = pp.get_alpha(x, y, yaw, tx, ty)
+    print(alpha)
+    circle1 = plt.Circle((x, y), l, color='r', fill=False)
+    plt.gcf().gca().add_artist(circle1)
+
+
+
+    plt.plot(x,y,'o')
+    plt.plot([x, tx], [y, ty])
     plt.show()
+
+    return path
 
 
 np.random.seed(int(time.time()))
 
-path_x = np.linspace(0, 2*np.pi, 10)
-path_y = np.sin(path_x)
+#path_x = np.linspace(0, 2*np.pi, 10)
+#path_y = np.sin(path_x)
+
+path_x = [0, 1, 2]
+path_y = [1.5, 2, 1.5]
+
+plt.xlim([0, 3])
+plt.ylim([0, 3])
 
 
 create_path_from_points(path_x, path_y)
--- pathfinding/pure_pursuit.py
+++ pathfinding/pure_pursuit.py
@@ -1,17 +1,44 @@
 import math
+import matplotlib.pyplot as plt
 
 lookahead = 0.2
 
 def curvature(l, alpha):
     return (2*math.sin(alpha))/l
 
-def get_control_signal(x, y, yaw, target):
+def get_alpha(x, y, yaw, tx, ty):
+    alpha = math.atan2(ty - y, tx - x) - yaw
+    return alpha
 
-    alpha = math.atan2(target.y - y, target.x - x) - yaw
+def find_nearest_point(x,y, l, path):
+
+    dx = [tx - x for tx in path.x]
+    dy = [ty - y for ty in path.y]
+
+    d = [x**2 + y**2 for x, y in zip(dx, dy)]
+    map(math.sqrt, d)
+
+    closed_point_index = d.index(min(d))
+
+    d = 0
+    i = closed_point_index
+
+    plt.plot([x,path.x[closed_point_index]], [y, path.y[closed_point_index]])
+
+    while d < l and (i + 1) < len(path.x):
+
+        dx = x - path.x[i]
+        dy = y - path.y[i]
+        d = math.hypot(dx, dy)
+
+        if d < l:
+            i += 1
+
+    return [path.x[i], path.y[i]]
 
 
-def find_nearest_point(x,y, path):
-
-    d_x = []
-    d_y =
+def get_control_signal(x, y, l, yaw, path):
+   tx, ty = find_nearest_point(x, y, l, path)
+   return get_alpha(x, y, tx, ty, yaw)
+    
 
