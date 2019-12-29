7c2e53f70ae416899ece6bf8d8f78d4c64349fe6
Jakob <jakobnnyberg@gmail.com>
2019-12-29 11:24:32 +0100

    use settings in kalman man

--- kalman/kalman_man.py
+++ kalman/kalman_man.py
@@ -19,8 +19,7 @@ async def kalman_man(context: Context):
         context.new_measurement_event.clear()
 
         data_logger = DataLogger()
-        position_estimator = PositionEstimator(std_dev_acc=0.8, std_dev_position=0.25, std_dev_velocity=0.8, dim_u=0,
-                                               dim_x=6, update_delay=0.2)
+        position_estimator = PositionEstimator(**context.settings["kalman"])
 
         position_estimator.start_kalman_filter(loc_data)
 
