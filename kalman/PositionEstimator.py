bf681294df9e579798d55ebe9375c53a51d0d6da
Torun <torun.nicander@gmail.com>
2020-01-07 10:55:01 +0100

    little kalman works again

--- kalman/PositionEstimator.py
+++ kalman/PositionEstimator.py
@@ -5,8 +5,7 @@ class PositionEstimator:
 
     def __init__(self, process_dev_pos, process_dev_acc, process_dev_heading, process_dev_heading_acc,
                  process_dev_vel,
-                 meas_dev_pos, meas_dev_heading, meas_dev_acc, speed_div_by_length, variable_dt, dim_z = 5, dim_u=0,
-                 dim_x=6, update_delay=0.1):
+                 meas_dev_pos, meas_dev_heading, meas_dev_acc, speed_div_by_length, variable_dt, uwb_only_kf, update_delay=0.1):
         self.process_var_acc = process_dev_acc
         self.process_var_vel = process_dev_vel
         self.process_var_heading = process_dev_heading
@@ -15,9 +14,18 @@ class PositionEstimator:
         self.meas_var_pos = meas_dev_pos
         self.meas_var_heading = meas_dev_heading
         self.meas_var_acc = meas_dev_acc
-        self.dim_u = dim_u
-        self.dim_x = dim_x
-        self.dim_z = dim_z
+
+        if uwb_only_kf:
+            self.dim_u = 0
+            self.dim_x = 4
+            self.dim_z = 2
+            self.use_acc = False
+        else:
+            self.dim_u = 1
+            self.dim_x = 8
+            self.dim_z = 5
+            self.use_acc = True
+
         self.update_delay = update_delay
         self.kf = None
         self.estimated_state = None
@@ -29,7 +37,7 @@ class PositionEstimator:
         self.kf = init_kalman_filter(loc_data, process_var_pos=self.process_var_pos, process_var_acc=self.process_var_acc,
                                      process_var_heading=self.process_var_heading,
                                      process_var_heading_acc=self.process_var_heading_acc,
-                                     process_var_vel=self.process_var_vel, dt=self.update_delay, dim_x=self.dim_x, dim_z=self.dim_z, dim_u=self.dim_u, use_acc=True,
+                                     process_var_vel=self.process_var_vel, dt=self.update_delay, dim_x=self.dim_x, dim_z=self.dim_z, dim_u=self.dim_u, use_acc=self.use_acc,
                                      speed_div_by_length=self.speed_div_by_length,
                                      meas_var_acc=self.meas_var_acc, meas_var_heading=self.meas_var_heading, meas_var_pos=self.meas_var_pos
                                      )
@@ -50,7 +58,7 @@ class PositionEstimator:
                                               process_var_acc=self.process_var_acc,
                                               process_var_velocity=self.process_var_vel,
                                               u=control_signal,
-                                              timestep=d_t, use_acc=True)
+                                              timestep=d_t, use_acc=self.use_acc)
         self.time = time.time()
         return self.estimated_state
 
