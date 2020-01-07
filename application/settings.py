1324e38a9dbf589063e98ddbf827e67d28984770
Jakob <jakobnnyberg@gmail.com>
2020-01-07 09:45:09 +0100

    add default settings

--- application/settings.py
+++ application/settings.py
@@ -6,12 +6,17 @@ DEFAULT_SETTINGS = {
     "goal_threshold": 0.35,
     "generate_movie": True,
     "kalman": {
-        "std_dev_acc": 0.8,
-        "std_dev_position": 0.25,
-        "std_dev_velocity": 0.8,
-        "dim_u": 0,
-        "dim_x": 6,
-        "update_delay": 0.2
+        "dim_u": 1,
+        "dim_x": 8,
+        "update_delay": 0.1,
+        "process_dev_pos": 0,
+        "process_dev_acc": 0.08,
+        "process_dev_heading": 0.0,
+        "process_dev_heading_acc": 0.08,
+        "process_dev_vel": 0.1,
+        "meas_dev_pos": 0.3,
+        "meas_dev_heading": 0.08,
+        "meas_dev_acc": 1.2,
     },
     "path": {
         "x": [],
@@ -27,7 +32,8 @@ def load_settings(filename):
     with open(filename) as f:
         try:
             loaded_settings = json.load(f)
-        except json.JSONDecodeError:
+        except json.JSONDecodeError as e:
+            print(e)
             print(f"Loading {filename} failed, using default settings.")
             return settings
     for key in loaded_settings:
